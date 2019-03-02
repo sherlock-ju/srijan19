bc76051e0afaaa42eeb4318b0a2b4aefb5965e7f
sherlock <sherlock.ju2k19@gmail.com>
2019-03-02 12:30:20 +0530

    completed

---
+++ index.js
@@ -0,0 +1,19 @@
+function submitClick(){
+    const pass = document.getElementById('contest-pass').value;
+    const url = document.getElementById('contest-url').value;
+    
+    if(pass === "CqHmlsHf"){
+        document.getElementById('google-form').setAttribute("src", `https://${url}?embedded=true`);
+        document.getElementById('google-form-div').style.display = "block";
+        document.getElementsByClassName('forms')[0].style.display = "none";
+        document.getElementsByClassName('main-body')[0].setAttribute("height", "100%");
+        setTimeout(() => {
+            alert("Your time is up! Please submit the test within '30 sec' otherwise you will loose your progress!");
+        }, 2400000);
+        setTimeout(() => {
+            document.getElementById('google-form-div').style.display = "none";
+            document.getElementsByClassName('thank-you')[0].style.display = "block";
+            window.location.href = "http://google.com";
+        }, 60000);
+    }
+}
